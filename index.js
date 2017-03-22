1cbe858901fc808faa443a3b56a76c12f053f15e
Keifer.Gu <keifergu@gmail.com>
2017-03-22 21:07:38 +0800

    完成基本代码；包扩 then 函数， doResolve 函数

--- index.js
+++ index.js
@@ -6,63 +6,194 @@ var validStates = {
 	REJECTED: 2
 }
 
-isFunction: function(val) {
-	return val && typeof val === "function";
+var Utils = {
+	isFunction: function (val) {
+		return val && typeof val === "function";
+	},
+	isObject: function (obj) {
+		return obj && typeof obj === "object"
+	},
+	isPromise: function (val) {
+    return val && val.constructor === KPromise;
+  },
+	isValidState: function(state) {
+		return (
+			(state === validStates.PENDING) ||
+      (state === validStates.REJECTED) ||
+      (state === validStates.FULFILLED))
+	},
+	runAsync: function (func) {
+		setTimeout(func, 0)
+	}
 }
 
-KPomise.prototype.transition = function (state, value) {
-	if(this.state === state ||this.state !== validStates.PENDING)
-		throw('Must from PENDING to other state')
-	if (state === validStates.FULFILLED && value)
-		throw('FULFILLED must have value')
-	if (state === validStates.REJECTED && value)
-		throw('REJECTED must have reason')
+var	transition = function (state, value) {
+	if (
+			this.state === state ||
+	    arguments.length !== 2 ||
+	    !Utils.isValidState(state) ||
+	    this.state !== validStates.PENDING) {
+		return ;
+	}
 
-	this.state = state;
-	this.value = value;
+	this.state = state
+	this.value = value
+	this.process()
 }
 
-var KPomise = function (fn) {
+var process = function () {
+	var that = this;
+	var fulfillFallBack = function (value) {
+       return value;
+     },
+     rejectFallBack = function (reason) {
+       throw reason;
+     }
+	// 最开始添加的 then 函数，此时 pengding 状态，直接返回
+	if (this.state === validStates.PENDING) return
+	Utils.runAsync(function() {
+		while(that.queue.length) {
+			var promiseItem = that.queue.shift(),
+				handler = null,
+				value
+			// 根据设置的状态调用对应的函数
+			if (that.state === validStates.FULFILLED)
+				handler = promiseItem.handlers.onFulfilled || fulfillFallBack // 如果对应的函数没有设置的话，则使用默认函数
+			if (that.state === validStates.REJECTED)
+				handler = promiseItem.handlers.onRejected || rejectFallBack
+			try {
+				value = handler(that.value)
+			} catch(e) {
+				promiseItem.reject(e)
+				continue
+			}
+
+			doResolve(promiseItem, value)
+		}
+	})
+}
+
+var doResolve = function (promise, x) {
+
+	if (promise === x) {
+		var msg = "can't resolve the same Promise"
+		promise.reject(new TypeError(msg))
+	}
+	// 当 resolve 的值 x 也是 promise 的时候
+	// 当前 promise 的 state 则依赖 x 的 state
+	else if (Utils.isPromise(x)) {
+		if (x.state === validStates.PENDING) {
+			// 在依赖的 promise 后添加 then 方法
+			// 当其状态改变时，使用该 then 方法 改变当前 promise 状态
+			x.then(function(val) {
+				doResolve(promise, val)
+			}, function(reason) {
+				promise.reject(reason)
+			})
+		} else {
+			// 当依赖的 promise 不是 PENDING 状态时
+			// 则直接将其值和状态传递到当前 promise
+			this.transition(x.state, x.value)
+		}
+	}
+	// 处理当 x 是对象或函数的情况
+	else if (Utils.isObject(x) || Utils.isFunction(x)) {
+		var called = false,
+			thenHandler
+		try {
+			thenHandler = x.then
+			if (Utils.isFunction(thenHandler)) {
+				thenHandler.call(x,
+					function (y) {
+						if (!called) {
+							doResolve(promise, y)
+							called = true
+						}
+					}, 
+					function(r) {
+						if (!called) {
+							promise.reject(r)
+							called = true
+						}
+					}
+				)
+			} else {
+				// then 不是函数 则以 x 为参数执行 promise
+				promise.resolve(x)
+				called = true
+			}
+		} catch(e) {
+			// 取 x.then 值或调用 x.then 时抛出异常，
+			// 则以该异常拒绝 promise
+			if (!called) {
+				promise.reject(e)
+				called = true
+			}
+		}
+	}
+	// 当 x 不是对象或者函数时，以 x 为参数执行 promise
+	else {
+		promise.resolve(x)
+	}
+}
+
+function KPromise (func) {
 	var that = this;
 	this.value = undefined;
 	this.state = validStates.PENDING;
 	this.queue = [];
-	fn(function resolve(value) {
-		doResolve(that, value)
-	}, function reject(error) {
-		doReject(that, error)
-	})
+	if (Utils.isFunction(func)) {
+		func(function (value) {
+			doResolve(that, value)
+		}, function (err) {
+			that.reject(err)
+		})
+	}
 };
 
-var doResolve = function (that, value) {
-	that.state = validStates.FULFILLED;
-	that.queue.reduce(function(acc, cuv) {
-		return cuv(acc)
-	}, value)
-}
-
-var doReject = function (that, error) {
-	
-}
-
-
-KPomise.prototype.then = function (onFulfilled, onRejected) {
-	if (isFunction(onFulfilled)) {
-		
+KPromise.prototype.then = function (onFulfilled, onRejected) {
+	var	promise = new this.constructor()
+	promise.handlers = {
+		onFulfilled: null,
+		onRejected: null
 	}
-	if (isFunction(onRejected)) {}
+	// 每个子 promise 会具有这两个属性
+	// 代表的是通过 then 方法添加的回调
+	if (Utils.isFunction(onFulfilled)) {
+		promise.handlers.onFulfilled = onFulfilled
+	}
+	if (Utils.isFunction(onRejected)) {
+		promise.handlers.onRejected = onRejected
+	}
 
-	this.queue.push(onFulfilled)
+	this.queue.push(promise)
+	this.process()
+	return promise
 }
+
+KPromise.prototype.catch = function(onRejected) {
+	return this.then(null, onRejected)
+}
+
+KPromise.prototype.resolve = function(value) {
+	this.transition(validStates.FULFILLED, value)
+}
+
+KPromise.prototype.reject = function(reason) {
+	this.transition((validStates.REJECTED, reason))
+}
+
+KPromise.prototype.transition = transition
+KPromise.prototype.process = process
 
 module.exports = {
     resolved: function (value) {
-        return new KPomise(function (resolve) {
+        return new KPromise(function (resolve) {
             resolve(value);
         });
     },
     rejected: function (reason) {
-        return new KPomise(function (resolve, reject) {
+        return new KPromise(function (resolve, reject) {
             reject(reason);
         });
     },
@@ -70,7 +201,7 @@ module.exports = {
         var resolve, reject;
 
         return {
-            promise: new KPomise(function (rslv, rjct) {
+            promise: new KPromise(function (rslv, rjct) {
                 resolve = rslv;
                 reject = rjct;
             }),
