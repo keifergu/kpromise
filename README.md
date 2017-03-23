23fdc4cf449ee74f33b855e3715123664c937785
Keifer.Gu <keifergu@gmail.com>
2017-03-23 20:00:23 +0800

    添加完整注释；修改 readme

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # KPromise
 ------
-KPromise 库是一个对 Promise/A+ 规范的实现，通过了 [Promise/A+ 测试集](https://github.com/promises-aplus/promises-tests),代码中包含大量的注释，帮助你理解 Promise 内部的实现机制。
-![](https://rawgit.com/promises-aplus/promises-spec/master/logo.svg)
+KPromise 是一个对 Promise/A+ 规范的实现，通过了 [Promise/A+ 测试集](https://github.com/promises-aplus/promises-tests),代码中包含注释，帮助你理解 Promise 内部的实现机制。
--- index.js
+++ index.js
@@ -28,6 +28,14 @@ var Utils = {
 	}
 }
 
+/**
+ * 对 promise 的状态进行改变的函数，改变后会自动检测状态并执行
+ * 
+ * @param  {Promise} promise 								需要改变状态的 promise
+ * @param  {State} state   									变更为该状态
+ * @param  {Promise|Thenable|value} value   resolve 的 value， reject 的 reason
+ * @return {}         											不符合变更状态的条件是直接停止运行
+ */
 var transition = function (promise, state, value) {
 	if (
 			promise.state === state ||
@@ -39,46 +47,74 @@ var transition = function (promise, state, value) {
 
 	promise.state = state;
 	promise.value = value;
+
+	// 每一次改变状态后执行一次 runner 函数
 	runner(promise);
 }
 
+/**
+ * 检测当前状态，如果不为 PENDING，则根据状态不同调用事先注册的函数
+ * 所有的 then 方法中注册的函数均在此处执行
+ * 
+ * @param  {Promise} promise 	要执行的 promise
+ * @return {}         				无返回值
+ */
 var runner = function (promise) {
-	var that = promise;
-	var fulfillFallBack = function (value) {
-       return value;
-     },
-     rejectFallBack = function (reason) {
-       throw reason;
-     };
-	// 最开始添加的 then 函数，此时 pengding 状态，直接返回
-	if (that.state === validStates.PENDING) {
+	var promise1 = promise;
+
+	if (promise1.state === validStates.PENDING) {
 		return ;
 	}
 
+	// 根据规范： onFulfilled 和 onRejected 只有在执行环境堆栈仅包含平台代码时才可被调用
+	// 此处使用工具中的 runAsync 实现
 	Utils.runAsync(function () {
-		while(that.queue.length) {
-			var promiseItem = that.queue.shift(),
+		while(promise1.queue.length) {
+			var promise2 = promise1.queue.shift(),
 				handler = null,
 				value
 			// 根据设置的状态调用对应的函数
-			if (that.state === validStates.FULFILLED) {
-				handler = promiseItem.handlers.onFulfilled || fulfillFallBack; // 如果对应的函数没有设置的话，则使用默认函数
-			} else if (that.state === validStates.REJECTED) {
-				handler = promiseItem.handlers.onRejected || rejectFallBack;
+			if (promise1.state === validStates.FULFILLED) {
+				// 根据规范：如果 onFulfilled 不是函数且 promise1 成功执行， promise2 必须成功执行并返回相同的值
+				// 此时 promise1 已经成功执行，如果 promise2 的 onFulfilled 不是函数，我们则直接使用该默认函数，返回相同值
+				handler = Utils.isFunction(promise2.onFulfilled) ?
+					promise2.onFulfilled :
+					function(v) { return v };
+			} else if (promise1.state === validStates.REJECTED) {
+				// 根据规范： 如果 onRejected 不是函数且 promise1 拒绝执行， promise2 必须拒绝执行并返回相同的据因
+				// 此时状态为 rejected ，promsie1 已经拒绝执行，如果 promise2 的 onRejected 不是函数，则直接使用相同原因拒绝执行
+				handler = Utils.isFunction(promise2.onRejected) ?
+					promise2.onRejected :
+					function(e) { throw e };
 			}
 
+			// 根据规范： 如果 onFulfilled 或者 onRejected 抛出一个异常 e ，则 promise2 必须拒绝执行，并返回拒因 e
 			try {
-				value = handler(that.value)
+				// 根据规范： onFulfilled 和 onRejected 必须被作为函数调用（即没有 this 值）
+				value = handler(promise1.value)
 			} catch(e) {
-				promiseItem.reject(e)
+				// 此时捕捉到执行期的异常，以此 reject promise2
+				promise2.reject(e)
+				// 继续运行后续的 promise
 				continue
 			}
-
-			doResolve(promiseItem, value)
+			// 根据规范： 如果 onFulfilled 或者 onRejected 返回一个值 x ，则运行下面的 Promise 解决过程：[[Resolve]](promise2, x)
+			// 执行 Resolve 过程
+			doResolve(promise2, value)
 		}
 	})
 }
 
+/**
+ * 核心的 Resolve 函数，需输入一个 promise 和一个值，我们表示为 [[Resolve]](promise, x)，
+ * 如果 x 有 then 方法且看上去像一个 Promise ，解决程序即尝试使 promise 接受 x 的状态；否则其用 x 的值来执行 promise 。
+ * 这种 thenable 的特性使得 Promise 的实现更具有通用性：只要其暴露出一个遵循 Promise/A+ 协议的 then 方法即可；
+ * 这同时也使遵循 Promise/A+ 规范的实现可以与那些不太规范但可用的实现能良好共存。
+ * 
+ * @param  {Promise} 					promise 需要处理的 promise
+ * @param  {Value | Thenable} x       resolve 的值
+ * @return {}        									无返回值
+ */
 var doResolve = function (promise, x) {
 
 	if (promise === x) {
@@ -148,10 +184,6 @@ function KPromise (func) {
 	this.value = undefined;
 	this.state = validStates.PENDING;
 	this.queue = [];
-	this.handlers = {
-		onFulfilled: null,
-		onRejected: null
-	}
 	if (Utils.isFunction(func)) {
 		func(function (value) {
 			doResolve(that, value)
@@ -165,12 +197,8 @@ KPromise.prototype.then = function (onFulfilled, onRejected) {
 	var	promise = new KPromise();
 	// 每个子 promise 会具有这两个属性
 	// 代表的是通过 then 方法添加的回调
-	if (Utils.isFunction(onFulfilled)) {
-		promise.handlers.onFulfilled = onFulfilled;
-	}
-	if (Utils.isFunction(onRejected)) {
-		promise.handlers.onRejected = onRejected;
-	}
+	promise.onFulfilled = onFulfilled;
+	promise.onRejected = onRejected;
 
 	this.queue.push(promise);
 	runner(this);
